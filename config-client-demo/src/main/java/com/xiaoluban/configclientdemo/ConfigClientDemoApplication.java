e003b4ec4cdb63753f343af76bb0184979cde35c
FrashtoGoat <1271573554@qq.com>
2023-02-15 10:49:49 +0800

    springboot-starter编写

--- config-client-demo/src/main/java/com/xiaoluban/configclientdemo/ConfigClientDemoApplication.java
+++ config-client-demo/src/main/java/com/xiaoluban/configclientdemo/ConfigClientDemoApplication.java
@@ -1,20 +1,20 @@
 package com.xiaoluban.configclientdemo;
 
+import com.demo.planemachines.factory.AcmeFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
+
 @SpringBootApplication
 public class ConfigClientDemoApplication {
 
+
     @Value("${name")
     private String name;
-
-    @RequestMapping("/name")
-    public String home() {
-        return name;
-    }
 
 
     public static void main(String[] args) {
